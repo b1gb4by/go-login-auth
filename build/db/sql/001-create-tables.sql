9fc0eaa916da369732c234fbbd55ec8fe84c86ba
morero <taisuke.1202morero@gmail.com>
2021-06-27 15:07:52 +0900

    ♻️ SQLファイルを修正

--- build/db/sql/001-create-tables.sql
+++ build/db/sql/001-create-tables.sql
@@ -1,4 +1,10 @@
 ---- DROP ----
+drop schema if exists go_auth;
+
+create schema go_auth;
+
+use go_auth;
+
 drop table if exists `users`;
 
 ---- CREATE ----
@@ -12,3 +18,24 @@ create table if not exists `users` (
     `updated_at` default current_timestamp on update current_timestamp,
     primary key(`id`)
 ) default charset = utf8 collate = utf8_bin;
+
+---- INSERT ----
+insert into
+    users (first_name, last_name, email, password)
+values
+    (
+        "Alan",
+        "Bob",
+        "alan@example.com",
+        "test_password"
+    );
+
+insert into
+    users (first_name, last_name, email, password)
+values
+    (
+        "Callen",
+        "Den",
+        "callen@example.com",
+        "test_password1"
+    );
