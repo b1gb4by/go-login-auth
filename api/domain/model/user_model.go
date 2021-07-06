20225e67d1ad8fa51800158a88e462afdcd50112
Taisuke Okamoto <taisuke@papelook.com>
2021-07-06 11:28:34 +0900

    ♻️ JSONのタグを追記

--- api/domain/model/user_model.go
+++ api/domain/model/user_model.go
@@ -8,9 +8,9 @@ type User struct {
 }
 
 type AcquisitionUser struct {
-	ID        int    `gorm:"column:id"`
-	FirstName string `gorm:"column:first_name"`
-	LastName  string `gorm:"column:last_name"`
-	Email     string `gorm:"column:email"`
-	Password  []byte `gorm:"column:password"`
+	ID        int    `json:"id" gorm:"column:id"`
+	FirstName string `json:"first_name" gorm:"column:first_name"`
+	LastName  string `json:"last_name" gorm:"column:last_name"`
+	Email     string `json:"email" gorm:"column:email"`
+	Password  []byte `json:"-" gorm:"column:password"`
 }
