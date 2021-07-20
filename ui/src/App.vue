bc1bfe3914303caa04630e3a4eb661485ebdc914
morero <taisuke.1202morero@gmail.com>
2021-07-20 10:57:29 +0900

    ♻️ 新たに雛形を作成

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,14 +1,37 @@
 <template>
   <Nav />
- 
+
   <router-view />
 </template>
- 
-<script>
-import Nav from "@/components/Nav"
-export default {
-  components: {
-    Nav
-  }
+
+<script lang="ts">
+import Nav from '@/components/Nav'
+export default{
+    components: {
+        Nav
+    }
 }
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
--- ui/src/views/Login.vue
+++ ui/src/views/Login.vue
@@ -1,12 +1,9 @@
 <template>
     Login
 </template>
- 
+
 <script>
 export default {
     name: "Login"
 }
 </script>
-
-<style>
-</style>
